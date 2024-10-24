155021c4ae3c7ae32a0ac9a6dd71ce51b4eae347
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-23 22:09:21 -0300

    CREATED APIS

---
+++ Backend/src/api/Produto.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const rotas = express.Router();
+
+const ProdutoControllers = require('../controllers/Produto');
+const storage = require('../middewares/produtos/multerConfig');
+
+const multer = require('multer');
+const upload = multer({ storage: storage });
+
+
+rotas.get('/', ProdutoControllers.listarProduto);
+
+rotas.post('/', upload.single('imagem'), ProdutoControllers.inserirProduto);
+
+rotas.put('/:id', upload.single('imagem'), ProdutoControllers.editarProduto);
+
+rotas.delete('/:id', ProdutoControllers.excluirProduto);
+
+module.exports = rotas;
---
+++ Backend/src/api/Users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const rotas = express.Router();
+
+const UsersControllers = require('../controllers/Users');
+const validationCreateLogin = require('../middewares/Users/validationCreateLogin');
+const validationLogin = require('../middewares/Users/validationLogin');
+const userLogin = require('../middewares/Users/userLogin');
+const checktoken = require('../middewares/Users/checkToken');
+const session = require('../middewares/Users/session');
+
+rotas.get('/', UsersControllers.listarUsers);
+
+rotas.get('/usuario/:id', checktoken, userLogin);
+
+rotas.post('/auth/createuser', validationCreateLogin, UsersControllers.inserirUsers);
+
+rotas.post('/auth/login', validationLogin, session);
+
+rotas.put('/:id', UsersControllers.editarUsers);
+
+rotas.delete('/:id', UsersControllers.excluirUsers);
+
+module.exports = rotas;
